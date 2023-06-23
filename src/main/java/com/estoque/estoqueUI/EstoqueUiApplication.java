faacc1d574a6d6bdb1fee0a04eb8610ad9b9ec10
Vini27Br <viniciusouza0@gmail.com>
2023-06-23 11:41:50 -0300

    commit incial

---
+++ src/main/java/com/estoque/estoqueUI/EstoqueUiApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.estoqueUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoque/estoqueUI/EstoqueUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.estoqueUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
